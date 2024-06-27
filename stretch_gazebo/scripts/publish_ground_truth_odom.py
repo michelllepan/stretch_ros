85fa452c8f2704e5f691aedf56f8d8089a7afc59
michelllepan <michellepan8@gmail.com>
2024-06-27 16:40:55 -0700

    Fix formatting

--- stretch_gazebo/scripts/publish_ground_truth_odom.py
+++ stretch_gazebo/scripts/publish_ground_truth_odom.py
@@ -19,7 +19,7 @@ odom=Odometry()
 header = Header()
 header.frame_id='/ground_truth'
 model = GetModelStateRequest()
-model.model_name='robot1'
+model.model_name='robot_1'
 models = []
 r = rospy.Rate(20)
 
